adca0f0b8b996b237daba18752c1dd4488035971
Amit <learnniggo981@gmail.com>
2025-06-12 09:11:28 +0530

    added two new routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Product";
+
+// this will take an array
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/product",
+    element: <ProductPage />,
+  },
+]);
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Amit</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
